8984709c53a8f32d30bc4db58aa8ea33b0ef8996
Javier Moreno <javiermr@permoda.com.co>
2025-09-06 22:28:39 -0500

    Proceso de pruebas unitarias

---
+++ PruebaTecnica.Test/PruebaTecnica.Api/Controller/CreateControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica.Test.PruebaTecnica.Api.Controller
+{
+    internal class CreateControllerTest
+    {
+    }
+}
